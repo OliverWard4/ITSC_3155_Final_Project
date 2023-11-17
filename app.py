ba420ba35051d877e4c95ed9e1d90eda8f4c4482
ssomo <sofia.mohamed.733@gmail.com>
2023-11-17 15:19:24 -0500

    header and footer fixed

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ def index():
     #Renders the home page
     return render_template('index.html')
 
+#Sign up page route
+@app.route('/signup')
+def signUp():
+    #Renders the sign up page
+    return render_template('signup.html')
+
+#Login page route
+@app.route('/login')
+def login():
+    #Renders the login page
+    return render_template('login.html')
+
 if __name__ == "__main__":
     #Runs the application
     app.run(debug=True)
