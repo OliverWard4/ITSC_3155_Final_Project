14c6abe3d73f9bda809c612a2f4f563415e6dc7d
richcomo123 <richcomo123@gmail.com>
2023-11-17 22:51:36 -0500

    Post will go here

--- corkboard/routes.py
+++ corkboard/routes.py
@@ -11,13 +11,13 @@ from flask_login import login_user, current_user, logout_user, login_required
 posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
+        'title': 'Board Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog Post 2',
+        'title': 'Board Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
